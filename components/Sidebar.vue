6d79c9db83be889795740a7ca9b36aef2e1dd4ba
Timothy Angus <timangus321@gmail.com>
2023-10-22 22:35:37 +0200

    redo layout contd

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,6 +1,15 @@
 <template>
-  <aside class="h-full flex basis-40 text-textDefault bg-secondary">
-    <div class="flex flex-col gap-4 px-5">
+  <aside class="h-full flex basis-20 text-textDefault bg-secondary">
+    <div class="flex flex-col-reverse gap-8 p-5">
+      <a href="https://www.linkedin.com/in/timothy-angus-b94969113/">
+        <img src="@/assets/icons/linkedin-original.svg" alt="Linkedin" />
+      </a>
+      <a href="https://github.com/TimAngus321">
+        <img src="@/assets/icons/github-original.svg" alt="Github" />
+      </a>
+      <a href="https://www.tim-angus.com/">
+        <img src="@/assets/icons/world-wide-web.png" alt="Personal Portfolio" />
+      </a>
     </div>
   </aside>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="bg-secondary h-screen flex flex-col">
     <Header />
-    <div class="h-full flex">
+    <div class="h-full flex pr-20">
       <Sidebar />
       <slot class="flex-grow"/>
     </div>
