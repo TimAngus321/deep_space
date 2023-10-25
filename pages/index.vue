9f5c15554c836d5e2a941830c1fad1e85368d29f
Timothy Angus <timangus321@gmail.com>
2023-10-25 21:46:57 +0200

    improve search page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,16 @@
-<!-- 
-    ToDo: Put the img and info into a nice card from Nuxt UI
- -->
-
 <template>
   <main class="flex flex-col h-full gap-6">
     <header class="flex justify-center">
       <h1 class="p-2 font-bold text-900">Nasa Image Search App</h1>
     </header>
 
-    <UContainer padding="0" class="flex flex-row w-full h-full items-center ">
+    <UContainer padding="0" class="flex flex-row w-full h-full items-center">
       <div class="flex flex-col p-2 gap-6 justify-center">
-        <h3>This is an app to find cool space images from Nasa's API</h3>
-        <p>What this app does</p>
+        <h3>
+          This app allows you to search for cool space images from Nasa's APIs.
+        </h3>
         <p>Purpose of this app:</p>
-        <p>ToDo</p>
+        <p>ToDo:</p>
       </div>
       <div v-if="isImg">
         <NuxtPicture
--- pages/space-image-search.vue
+++ pages/space-image-search.vue
@@ -3,29 +3,42 @@
     <header class="flex justify-center">
       <h1 class="p-2">Search Deep Space</h1>
     </header>
-    <section>
-      <UInput
-        icon="i-heroicons-magnifying-glass-20-solid"
-        size="sm"
-        color="white"
-        :trailing="false"
-        name="input"
-        placeholder="Search..."
-      />
-      <div class="grid grid-cols-4 gap-5"></div>
 
-    </section>
+    <UContainer padding="0" class="flex flex-row w-full h-full items-center">
+      <section>
+        <div>
+          <UInput
+            v-model="q"
+            name="q"
+            placeholder="Search..."
+            icon="i-heroicons-magnifying-glass-20-solid"
+            :ui="{ icon: { trailing: { pointer: '' } } }"
+          >
+            <template #trailing>
+              <UButton
+                v-show="q !== ''"
+                color="gray"
+                variant="link"
+                icon="i-heroicons-x-mark-20-solid"
+                :padded="false"
+                @click="q = ''"
+              />
+            </template>
+          </UInput>
+        </div>
+        <div class="grid grid-cols-4 gap-5"></div>
+      </section>
+    </UContainer>
   </main>
 </template>
 
 <script setup lang="ts">
+const q = ref('')
+// const url: string = "https://images-api.nasa.gov/search?q="
+// const searchQuery: string = "pillars"
 
-  // const url: string = "https://images-api.nasa.gov/search?q="
-  // const searchQuery: string = "pillars"
-
-  // const {data: images}: any =  await useFetch(`${url}${searchQuery}`)
-  // console.log('images ', images)
-
+// const {data: images}: any =  await useFetch(`${url}${searchQuery}`)
+// console.log('images ', images)
 </script>
 
 <style lang="scss" scoped></style>
