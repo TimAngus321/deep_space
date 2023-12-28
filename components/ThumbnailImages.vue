bfa11d1c373ac99988a5f1265bfe05639f9957b5
Timothy Angus <timangus321@gmail.com>
2023-12-28 22:05:23 +0000

    work on details page

--- components/ThumbnailImages.vue
+++ components/ThumbnailImages.vue
@@ -1,28 +1,25 @@
 <script setup lang="ts">
 const props = defineProps(["thumbnailInfo"]);
 console.log("thumbInfo:", props);
-// Request needed from Nasa (for large img etc): https://images-api.nasa.gov/asset/
-// <NuxtLink :to="`/imageDetails/${thumbnailInfo.nasa_id}`">
-//       <p class="btn my-4">View Details</p>
-//     </NuxtLink>
 </script>
 
 <template>
   <!-- Add grid-masonry to grid when it's more natively supported -->
-  <div v-if="thumbnailInfo && thumbnailInfo?.length" class="grid grid-cols-4 gap-5">
-  <figure
-    v-for="thumbData in thumbnailInfo"
+  <div
+    v-if="thumbnailInfo && thumbnailInfo?.length"
+    class="grid grid-cols-4 gap-5"
   >
-    <NuxtLink :to="`imageDetails/${thumbData.nasa_id}`">
-      <img
-        class="mb-5"
-        :key="thumbData?.nasa_id"
-        :src="thumbData?.thumbnail"
-        alt="Thumbnail"
-      />
-    </NuxtLink>
-  </figure>
-</div>
+    <figure v-for="thumbData in thumbnailInfo">
+      <NuxtLink :to="`imageDetails/${thumbData?.nasa_id}`">
+        <img
+          class="mb-5"
+          :key="thumbData?.nasa_id"
+          :src="thumbData?.thumbnail"
+          alt="Thumbnail"
+        />
+      </NuxtLink>
+    </figure>
+  </div>
 </template>
 
 <style scoped></style>
--- pages/imageDetails/[nasa_id].vue
+++ pages/imageDetails/[nasa_id].vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 const { nasa_id } = useRoute().params;
+console.log(nasa_id)
+
+// To pass data add a store Pinia 
+// Request needed from Nasa (for large img etc): https://images-api.nasa.gov/asset/
+
+
 // const uri = 'https://fakestoreapi.com/products/' + id
+
 </script>
 
 <template>
   <div>
     <h1>{{ nasa_id }}</h1>
+
   </div>
 </template>
 
