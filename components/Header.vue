8c52dd4344c95bf611abb4e768e64d4cd0f821e8
Timothy Angus <timangus321@gmail.com>
2024-04-18 17:46:47 +0200

    add responsiveness progress

--- components/Header.vue
+++ components/Header.vue
@@ -4,15 +4,15 @@
  -->
 <template>
   <header
-    class="ui-gray h-20 bg-gray-100 dark:bg-gray-800 text-textDefault bg-theme-gray sticky top-0 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60"
+    class="ui-gray h-20 bg-gray-100 dark:bg-gray-800 text-textDefault bg-theme-gray sticky top-0 z-50 border-b border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60"
   >
     <nav class="flex flex-row h-full w-full p-7 pr-10 items-center">
       <div class="flex h-full w-full items-center">
         <ColorModeBtn />
       </div>
       <div class="flex items-center justify-end w-full gap-6">
-        <NuxtLink to="/" class="hover:text-primary p-2">Home</NuxtLink>
-        <NuxtLink to="/nasaImgSearch" class="hover:text-primary p-2"
+        <NuxtLink to="/" class="hover:text-primary p-2 text-xs lg:text-lg">Home</NuxtLink>
+        <NuxtLink to="/nasaImgSearch" class="hover:text-primary p-2 text-xs lg:text-lg"
           >Image Search</NuxtLink
         >
         <UPopover
