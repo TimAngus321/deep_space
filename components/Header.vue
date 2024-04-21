ed2643e51f205d1ab74422260f03b85bf4b7149e
Timothy Angus <timangus321@gmail.com>
2024-04-21 15:51:24 +0200

    style fixes

--- components/Header.vue
+++ components/Header.vue
@@ -6,17 +6,17 @@
   <header
     class="ui-gray h-20 bg-gray-100 dark:bg-gray-800 text-textDefault bg-theme-gray sticky top-0 z-50 border-b border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60"
   >
-    <nav class="flex flex-row h-full w-full p-7 pr-10 items-center">
+    <nav class="flex flex-row h-full w-full p-5 pr-10 items-center">
       <div class="flex h-full w-full items-center">
         <ColorModeBtn />
       </div>
       <div class="flex items-center justify-end w-full gap-6">
-        <NuxtLink to="/" class="hover:text-primary p-2 text-xs lg:text-lg">Home</NuxtLink>
-        <NuxtLink to="/nasaImgSearch" class="hover:text-primary p-2 text-xs lg:text-lg"
+        <NuxtLink to="/" class="hover:text-primary p-2 text-sm lg:text-lg">Home</NuxtLink>
+        <NuxtLink to="/nasaImgSearch" class="hover:text-primary p-2 text-sm lg:text-lg"
           >Image Search</NuxtLink
         >
         <UPopover
-          mode="hover"
+          mode="click"
           :popper="{ placement: 'bottom-end', offsetDistance: 0 }"
         >
           <template #panel>
@@ -24,13 +24,13 @@
              <p>Profile Content Coming Soon</p>
             </div>
           </template>
-          <NuxtLink to="/" class="hover:text-primary p-2">
+          <div class="hover:text-primary p-2">
             <UAvatar
               class="flex align-middle"
               src="https://avatars.githubusercontent.com/u/739984?v=4"
               alt="Avatar"
             />
-          </NuxtLink>
+          </div>
         </UPopover>
       </div>
     </nav>
